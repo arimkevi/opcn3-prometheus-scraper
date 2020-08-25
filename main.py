4a1c1dabc714e4534e494f827708f2d1168a2bdc
Artur Rimkevitch <arimkevitch@reaktor.com>
2020-08-25 11:27:47 +0300

    Change hostname reader to platform

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import json
 import glob
 import signal
 import sys
-import socket
+import platform
 
 from opcn3 import Opcn3
 from prometheus_client import Gauge, start_http_server
 
 DEV_TTY='/dev/ttyACM0'
-LOCATION = socket.gethostname()
+LOCATION = platform.node()
 PROMETHEUS_PORT=8000
 MEASURING_DELAY=10
 
